0b210bf03eea7ba93d9ce7ff64709e5e2fbf40c1
reivax-boucoi <reivax.boucoi@laposte.net>
2025-09-03 01:43:45 +0200

    program update

--- split-flap_counter/SW/main.c
+++ split-flap_counter/SW/main.c
@@ -4,46 +4,51 @@
 #include <avr/io.h>
 #include<avr/interrupt.h>
 
-#define LEDG_HIGH (PORTD |= 1<<PIND4)
-#define LEDG_LOW (PORTD &=~(1<<PIND4))
-#define LEDG_TOGGLE (PORTD ^=(1<<PIND4))
+#define LEDY_HIGH (PORTD |= 1<<PIND0)
+#define LEDY_LOW (PORTD &=~(1<<PIND0))
+#define LEDY_TOGGLE (PORTD ^=(1<<PIND0))
 
-#define LEDR_HIGH (PORTD |= 1<<PIND5)
-#define LEDR_LOW (PORTD &=~(1<<PIND5))
-#define LEDR_TOGGLE (PORTD ^=(1<<PIND5))
+#define LEDR_HIGH (PORTD |= 1<<PIND1)
+#define LEDR_LOW (PORTD &=~(1<<PIND1))
+#define LEDR_TOGGLE (PORTD ^=(1<<PIND1))
+
+#define BTN1_STATE (PORTD & (1<<PIND4))
+#define BTN2_STATE (PORTD & (1<<PIND6))
 
 #define STEPPER_TIMER_CNT 155 // 4MHz/64/(155+1) = ~400Hz (stepper pulse rate)
 #define DAILY_TIMER_CNT 31249 // 4MHz/1024/(31249+1=8s
-#define DAILY_TIMER_OVF 10800 // 1 day = 10800 * 8s
+#define DAILY_TIMER_OVF 5//10800 // 1 day = 10800 * 8s
 
-#define STEPS_PER_FLAP 205 //2048=full turn, 205 steps per flap
+#define STEPS_PER_FLAP 202 //2048=full turn, 205 steps per flap
+#define ZERO_STEP_OFFSET 100
 
 #pragma message ("test")
 
 volatile uint8_t stepperState=0;        //Keep track of the stepper driving mode phase.
-volatile uint16_t remainingSteps=2048;  //Keep track of remaining steps to go
+volatile uint16_t remainingStepsM1=2048;  //Keep track of remaining steps to go
+volatile uint16_t remainingStepsM2=2048;  //Keep track of remaining steps to go
 volatile uint16_t timer1OVF_cnt=0;      //Time keeping
-volatile uint8_t zeroing=0;             //True when zeroing is ongoing.
-void increment();
+volatile uint8_t zeroing=0b11;             //True when zeroing is ongoing.
+volatile uint8_t btn_debounce=0x0F;
 
 int main(void){
-    DDRD |= (1 << PIND4) ;              //LEDG init
-    DDRD |= (1 << PIND5) ;              //LEDR init
-    DDRD&=~((1<<PIND2) | (1<<PIND3));   //INT0 & INT1 init for wheel 1&2 limit switches
+    DDRD |=  (1 << PIND0) | (1 << PIND1);              //LEDY & LEDR init as outputs
+    DDRD &=~((1 << PIND2) | (1 << PIND3) | (1 << PIND4) | (1 << PIND6));   //INT0 & INT1 init for wheel 1&2 limit switches inputs
+    PORTD=0b1011100;                    //Enable input internal pullups
     DDRB=0xFF;                          //PORTB drives the steppers (2*4 lines)
     PORTB=0;                            //Low=coils OFF
-    LEDG_LOW;
+    LEDY_LOW;
     LEDR_LOW;
 
     OCR0A = STEPPER_TIMER_CNT;  //~400Hz
     OCR1A = DAILY_TIMER_CNT;    // 8s
     TCCR0A = (1 << WGM01);
-    TCCR0B = (1 << CS01) | (1 << CS00);// prescaler 64, clear timer on OCR0A match
-    TCCR1B |= (1 << WGM12) | (1 << CS12) | (1 << CS10);// prescaler 1024, clear timer on OCR1A match
-    TIMSK |= (1 << OCIE1A) | (1 << OCIE0A); //Enable Timer compare interrupts
-    GIMSK |= (1<<INT1) | (1<<INT0);         //Enable INT0 & INT1 interrupts
-    MCUCR |=(1<<ISC11) | (1<<ISC01);        //Falling edges generates interrupts
-    sei();                                  //Global interurpt enable
+    TCCR0B = (1 << CS01)  | (1 << CS00);               // prescaler 64, clear timer on OCR0A match
+    TCCR1B|= (1 << WGM12) | (1 << CS12) | (1 << CS10); // prescaler 1024, clear timer on OCR1A match
+    TIMSK |= (1 << OCIE1A)| (1 << OCIE0A);             //Enable Timer compare interrupts
+    GIMSK |= (1 << INT1)  | (1 << INT0);               //Enable INT0 & INT1 interrupts
+    MCUCR |= (1 << ISC11) | (1 << ISC01);              //Falling edges generates interrupts
+    sei();                                              //Global interurpt enable
 
     while(1){
     }
@@ -52,33 +57,78 @@ int main(void){
 
 ISR (INT0_vect){//Unit wheel zero hit
     
+    if(zeroing & 0b01){
+        zeroing &=~ 1;
+        remainingStepsM1 = 2048 - ZERO_STEP_OFFSET;
+    }else{
+        remainingStepsM1 = ZERO_STEP_OFFSET;
+        remainingStepsM2 += STEPS_PER_FLAP;
+        LEDR_HIGH;
+    }
 
 }
 
 ISR (INT1_vect){//Tens wheel zero hit
-    LEDG_TOGGLE;
+    if(zeroing & 0b10){
+        zeroing &= ~0b10;
+        remainingStepsM2 = 2048 - ZERO_STEP_OFFSET;
+    }else{
+        remainingStepsM2 = ZERO_STEP_OFFSET;
+    }
 }
 
 
 ISR (TIMER1_COMPA_vect){// 8s counter for day keeping
-    if(++timer1OVF_cnt>=DAILY_TIMER_OVF){
-        timer1OVF_cnt=0;
-        increment();
+    if(++timer1OVF_cnt >= DAILY_TIMER_OVF){
+        timer1OVF_cnt = 0;
+        remainingStepsM1 += STEPS_PER_FLAP;
     }
 }
 
 ISR(TIMER0_COMPA_vect){
-    if(remainingSteps>0){
-        remainingSteps--;
-        if(--stepperState>3)stepperState=3;
-        PORTB=1<<stepperState;
+    if(BTN1_STATE){
+        if((btn_debounce & 0x0F) != 0x0F){
+            btn_debounce++;
+        }
     }else{
-        PORTB=0;
+        if((btn_debounce & 0x0F)!=0x0){
+            btn_debounce--;
+            if((btn_debounce & 0x0F) == 0x0){   //Reset counters
+                zeroing = 0b11;
+                remainingStepsM1 = 2048;
+                remainingStepsM2 = 2048;
+                timer1OVF_cnt = 0;
+            }
+        }
+    }
+    
+    if(BTN2_STATE){
+        if((btn_debounce & 0xF0) != 0xF0){
+            btn_debounce += 0x10;
+        }
+    }else{
+        if((btn_debounce & 0xF0) != 0x0){
+            btn_debounce -= 0x10;
+            if((btn_debounce & 0xF0) == 0x0)remainingStepsM1 += STEPS_PER_FLAP;
+        }
+    }
+    
+    if(remainingStepsM1 > 0){
+        remainingStepsM1--;
+        stepperState = ((stepperState & 0x0F) >> 1) | (stepperState & 0xF0);
+        if(stepperState & 0x0F == 0) stepperState = 0x08 | (stepperState & 0xF0);
+        PORTB = (stepperState & 0x0F) | (PORTB & 0xF0);
+    }else{
+        PORTB &= ~0x0F;
+    }
+    if(remainingStepsM2 > 0){
+        remainingStepsM2--;
+        stepperState = ((stepperState & 0xF0) >> 1)|(stepperState & 0x0F);
+        if(stepperState & 0xF0 == 0) stepperState = 0x80 | (stepperState & 0x0F);
+        PORTB = (stepperState & 0xF0) | (PORTB & 0x0F);
+    }else{
+        PORTB &= ~0xF0;
         LEDR_LOW;
     }
 }
-void increment(){
-    if(remainingSteps==0){
-        remainingSteps+=STEPS_PER_FLAP;
-    }
-}
+
